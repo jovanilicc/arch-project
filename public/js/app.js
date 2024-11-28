c44caee83357eb7cc2736c98ecba70b432e4ac50
Jovan Ilic <jovanim89.ilic@gmail.com>
2024-11-28 23:46:49 +0100

    Added to homepage image changing functionality

---
+++ public/js/app.js
@@ -0,0 +1,22 @@
+// Hero section image change
+let box = document.querySelectorAll(".box__item");
+let heroImg = document.querySelector(".hero-section");
+const activeBox = (e) => {
+  let currentElement = e.target;
+  let imageSrc = currentElement.getAttribute("data-imageSrc");
+  imageSrc = "/public/images/" + imageSrc + ".png";
+
+  if (currentElement.classList.contains("box__item--active")) {
+    return;
+  } else {
+    box.forEach((element) => {
+      element.classList.remove("box__item--active");
+    });
+    currentElement.classList.add("box__item--active");
+    heroImg.style.backgroundImage = "url('" + imageSrc + "')";
+  }
+};
+
+box.forEach((element) => {
+  element.addEventListener("click", activeBox);
+});
